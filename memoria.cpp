0568e9b5fad5050cb02c1e84d402b077b8253b79
mariabelarmino <maluml.alves@gmail.com>
2024-11-28 13:44:16 -0300

    Definindo o tamanho da memória em tempo de execução

---
+++ memoria.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int tamMemoria;
+    cout << "Insira o tamanho da memória:";
+    cin >> tamMemoria;
+    int *pont = new int[tamMemoria]; // Colocamos um ponteiro no inicio da memória dinâmica
+}
