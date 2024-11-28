f3cb8fefb421a0a4f78830882d512b24f2aa5158
mariabelarmino <maluml.alves@gmail.com>
2024-11-28 19:03:13 -0300

    Alocação de processos direto da entrada na memória virtual

---
+++ alocando_processos.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+struct Processo_{
+    int id;
+    int tamProcesso;
+    bool R;
+    bool M;
+}Processo;
+
+int alocacao_direta(Processo novo_processo){
+    int memoria_virtual[100000];
+    if (criar_processos){
+        for (int i = 0; novo_processo.tamProcesso != 0; i++){
+            memoria_virtual[i] = novo_processo.tamProcesso;
+            novo_processo.tamProcesso--;
+        }
+    }
+}
+
+int main(){
+
+}
