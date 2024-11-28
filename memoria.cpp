d24deed8632b9a0b4f7ce303ee48d68c5cf71448
mariabelarmino <maluml.alves@gmail.com>
2024-11-28 18:25:06 -0300

    Tirando a funcão da main

--- memoria.cpp
+++ memoria.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int tamMemoria;
+int definir_tamMemoria(int tamMemoria){
     cout << "Insira o tamanho da memória:";
     cin >> tamMemoria;
     int *pont = new int[tamMemoria]; // Colocamos um ponteiro no inicio da memória dinâmica
+}
+int main()
+{
+
 }
